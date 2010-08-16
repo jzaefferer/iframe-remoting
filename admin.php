0e22e630326f8bfacc8a9a4237bb601490f4aecc
jzaefferer <joern.zaefferer@gmail.com>
2010-08-16 19:13:44 +0200

    Added admin.php, simple interface for editing urls.txt

---
+++ admin.php
@@ -0,0 +1,48 @@
+<?php 
+$urls = file("urls.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+if (isset($_POST['url'])) {
+	array_unshift($urls, $_POST['url']);
+	$urls = array_unique($urls);
+	file_put_contents("urls.txt", implode("\n", $urls));
+	header("Location: ./admin.php");
+	die();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<meta charset=utf-8 />
+<title>iframe-remoting - nothing loaded, yet</title>
+<script type="text/javascript">
+	$(function() {
+		$("#history").click( function() {
+			$("#url").val( $(this).val() );
+		});
+	});
+</script>
+<style type="text/css">
+	
+</style>
+</head>
+<body>
+	<form method="post">
+		<fieldset>
+			<legend>Enter a url or select from history, then submit.</legend>
+			<div>
+				<input id="url" name="url" value="<?php echo $urls[0] ?>" />
+				<input type="submit" value="(Re)load" />
+			</div>
+			<div>
+				<select id="history" size="<?php echo count($urls) ?>">
+				<?php 
+					foreach ($urls as $url) {
+						echo "<option>" . $url . "</option>";
+					}
+				?>
+				</select>
+			</div>
+		</fieldset>
+	</form>
+</body>
+</html>
